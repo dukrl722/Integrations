f49af9454ac7f02bbfc7cca34c488925057f74a2
Lucas L de Souza <lucas@lucaslenoch.com.br>
2021-06-20 16:43:21 -0300

    Adicionado CSS disponibilicado pelo Figma

--- tasche-zeit/src/App.js
+++ tasche-zeit/src/App.js
@@ -5,12 +5,8 @@ function App() {
   return (
       <div className="App">
         <h1 className="title">Tasche Zeit</h1>
-        <h2>A simple and pratical time-manager for those who want to be organized</h2>
-        <p className="version">Currently on version 0.1</p>
-
-        <Login />
-
-        <p className="footer">UTFPR, Dois Vizinhos, Engenharia de Software, 4o periodo (2021)</p>
+        <h2 className="description">A simple and pratical time-manager for those who want to be organized</h2>
+        <Login className="login"/>
       </div>
   );
 }
--- tasche-zeit/src/components/login.js
+++ tasche-zeit/src/components/login.js
@@ -4,15 +4,17 @@ import './login.css';
 export default function Login(){
     return (
         <form action="" method="GET" className="pageLogin">
-            <div className="login">
-                <label>Login:</label>
-                <input className="login_input" type="text" placeholder="Type in your login account"></input>
-            </div>
-            <div className="password">
-                <label>Password:</label>
-                <input className="pass_input" type="password" placeholder="Type in your password"></input>
-            </div>
-            <button className="logButton">Send</button>
+            <label className="login_label">Login:</label>
+            <input className="login_input" type="text" placeholder="Type in your login account"></input>
+            <input className="pass_input" type="password" placeholder="Type in your password"></input>
+            <a href="www.google.com">Forgot your password?</a>
+            
+            <button className="continue">Continue</button>
+
+            <p className="signUp">
+                Dont have an account?
+                <button>Sign Up</button>
+            </p>
         </form>
     );
 }
