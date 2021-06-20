7fc1f574db8e6e833b7f2b003ab0b2ed200e6fb2
Lucas L de Souza <lucas@lucaslenoch.com.br>
2021-06-20 17:28:23 -0300

    Modificado CSS original do Figma para adaptar-se a tela do Chrome

--- tasche-zeit/src/App.js
+++ tasche-zeit/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
       <div className="App">
         <h1 className="title">Tasche Zeit</h1>
-        <h2 className="description">A simple and pratical time-manager for those who want to be organized</h2>
+        <h2 className="description">A simple and pratical time-manager <br/> for those who want to be organized</h2>
         <Login className="login"/>
       </div>
   );
--- tasche-zeit/src/components/login.js
+++ tasche-zeit/src/components/login.js
@@ -4,7 +4,7 @@ import './login.css';
 export default function Login(){
     return (
         <form action="" method="GET" className="pageLogin">
-            <label className="login_label">Login:</label>
+            <label className="login_label"><strong>Login</strong></label>
             <input className="login_input" type="text" placeholder="Type in your login account"></input>
             <input className="pass_input" type="password" placeholder="Type in your password"></input>
             <a href="www.google.com">Forgot your password?</a>
