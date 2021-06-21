aa08920d9376c4340717c5dd3ba21d351752c894
Lucas L de Souza <lucas@lucaslenoch.com.br>
2021-06-21 13:55:12 -0300

    Abandonado o uso de React Router momentaneamente

--- tasche-zeit/src/App.js
+++ tasche-zeit/src/App.js
@@ -1,13 +1,11 @@
 //import './App.css';
 import Login from './components/login';
 import Indice from './components/indice';
-import {Route, Link} from 'react-router-dom';
 
 function App() {
   return (
       <div className="App">
-        <Route exact path="/" component={Login} />
-        <Route exact path="/index" component={Indice} />
+        <Login />
       </div>
   );
 }
